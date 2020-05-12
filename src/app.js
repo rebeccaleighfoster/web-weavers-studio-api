873224813a25dd55b302dbfac520570ddc18836a
rebeccaleighfoster <rebecca.leigh.foster@gmail.com>
2020-05-12 15:31:02 -0500

    cors error

--- src/app.js
+++ src/app.js
@@ -20,6 +20,12 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(
     bodyParser.urlencoded({
       extended: true,
